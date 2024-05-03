5f1c491eeabed78a187fbe2c3de483647f5b9b0b
LargeJump <geschiele@gmail.com>
2024-05-03 03:02:41 -0700

    new version

--- javascript/leaflet.js
+++ javascript/leaflet.js
@@ -1,6 +1,8 @@
-var map = L.map('map').setView([38, -98], 3);
+var britmarCoord = [49.304132912228184, -123.00885722630613]
+var map = L.map('map').setView(britmarCoord, 16);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
-
+var britmarMarker = L.marker(britmarCoord).addTo(map);
+britmarMarker._icon.classList.add("britmar-marker");
